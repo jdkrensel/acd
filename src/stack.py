0b326d0797c68fa7358f7de7174ec84b02f5265f
Jesse Krensel <jdkrensel@gmail.com>
2025-10-17 20:12:47 -0400

    refactor(src): rename SrcStack to Stack and simplify app initialization

---
+++ src/stack.py
@@ -0,0 +1,11 @@
+from aws_cdk import (
+    Duration,
+    Stack,
+)
+from constructs import Construct
+
+class Stack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
